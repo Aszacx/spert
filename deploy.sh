c8e55e477537d5a9953f0fd58ace910cd9eed417
issac.centeno <issac.centeno@spert.com.mx>
2015-05-04 08:59:02 -0500

    Actualización 08:59am

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -ne "Mensaje de commit:"
+read msg "\n" 
+echo -ne "git commit -m $msg" "\n" 
+echo -ne "git push -u origin master" "\n" 
+echo -ne "Aszacx" "\n" 
+echo -ne "isacx92" "\n" 
