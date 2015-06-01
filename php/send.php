2422f8f1fb9c21df54bb6698bcb2f29dfb0a6514
issac.centeno <issac.centeno@spert.com.mx>
2015-06-01 01:54:46 -0500

    Actualización 01/06/15

--- php/send.php
+++ php/send.php
@@ -2,8 +2,8 @@
 	if(isset($_POST['email'])) {
 
 		// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-		$email_to = $_POST['email'];
-		$email_subject = "isacenteno@hotmail.com";
+		$email_to = "isacenteno@hotmail.com";
+		$email_subject = $_POST['email'];
 
 		// Aquí se deberían validar los datos ingresados por el usuario
 		if(!isset($_POST['first_name']) ||
