4e40c35a185c9fffc8b92f0da8050c4c9cea490d
issac.centeno <issac.centeno@spert.com.mx>
2015-06-01 02:23:27 -0500

    Actualización 01/06/15

--- views/contacto.tpl.php
+++ views/contacto.tpl.php
@@ -6,7 +6,7 @@
 	<div class="forma" align="center">
 		<h3>Escribemos</h3>
 		<p>En breve nos pondremos en contacto contigo.</p>
-		<form action="../php/send.php" method="post">
+		<form action="" method="post">
 			<input placeholder="Nombre(s)*" type="text" name="first_name" maxlength="50" size="35" required>
 			<br/><br/>
 			<input placeholder="Empresa" type="text" name="empresa" maxlength="50" size="35">
@@ -16,7 +16,12 @@
 			<textarea placeholder="Escribe aquí tu mensaje*" name="comments" maxlength="500" cols="35" rows="6" required></textarea><br/>
 			<label>Campos con * obligatorios.</label>
 			<br /><br />
-			<input type="submit" value="ENVIAR MENSAJE" class="btn">
+			<input type="submit" value="ENVIAR MENSAJE" class="btn" name="enviar">
 		</form>	
 	</div>
+	<?php
+        if(isset($_POST['enviar'])){
+            require("../php/send.php");
+        }
+     ?>
 
